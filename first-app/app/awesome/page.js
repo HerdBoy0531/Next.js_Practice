ac7a9f2d3f9524e235c97732b59c323da9306a54
qweiop2468 <mseunghyeon1222@gmail.com>
2025-12-23 23:34:46 +0900

    251223

---
+++ first-app/app/awesome/page.js
@@ -0,0 +1,5 @@
+export default function AwesomePage() {
+  return<main>
+    <h1>NextJS IS Awesome!</h1>
+  </main>
+}
---
+++ react-crash-course/src/App.jsx
@@ -0,0 +1,8 @@
+import Post from './components/Post.jsx'
+
+function App(){
+  return <Post />;
+  <h1>Hello World!</h1>;
+}
+
+export default App;
