b92a5c38f8599db3f3bc18f3b3799efb7c6a24ea
qweiop2468 <mseunghyeon1222@gmail.com>
2026-01-16 18:44:00 +0900

    260116

--- foodies-starting-project/app/meals/page.js
+++ foodies-starting-project/app/meals/page.js
@@ -1,12 +1,18 @@
+import { Suspense } from 'react'
 import Link from 'next/link'
 
 import classes from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
+async function Meals(){
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals}/>
+}
 
 export default async function MealsPage(){
-  const meals = await getMeals();
+  
 
   return (
     <>
@@ -24,7 +30,9 @@ export default async function MealsPage(){
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals}/>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}> 
+        <Meals />
+        </Suspense>
       </main>
     </>
   )
